7a711e49a5a71662091cb42efdad0a9692b560a9
meganallred <meg21allred@gmail.com>
2020-11-17 09:01:41 -0500

    adding listen

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const app = express();
 
+app.listen(process.env.PORT);
+//app.listen(3000);
 
 app.get('/', (req, res) => {
     console.log("Two thumbs up!");
